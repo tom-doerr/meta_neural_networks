bbff98f3488f0ee7f4db60f6bc5cb35fcc3424d1
Aleksandr Zuev <zuev08@gmail.com>
2020-12-11 08:09:12 +0100

    Add generate probabilities option
    Generate probabilities can give different results than generate class
    labels. We probably need to find out why.

--- generate_class_labels.py
+++ generate_class_labels.py
@@ -2,6 +2,7 @@ import os, shutil
 
 import numpy as np
 import torch
+from torch.nn.functional import softmax
 from argparse import ArgumentParser
 from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
@@ -32,24 +33,29 @@ def main(hparams):
         hparams.train = True
         hparams.test = True
 
+    if hparams.probabilities:
+        folder = 'probabilities'
+    else:
+        folder = 'labels'
+
     if hparams.train:
         train_dataset = CIFAR10(hparams.data_dir, train=True, download=False, transform=transform_dataset)
         train_dataloader = DataLoader(train_dataset, batch_size=hparams.batch_size, num_workers=4, shuffle=False, drop_last=False, pin_memory=True)
         print('Evaluate for train dataset')
-        labels = evaluate_for_dataset(module.model, train_dataloader, gpu=not hparams.no_gpu)
-        os.makedirs('labels', exist_ok=True)
-        file_path = os.path.join('labels', '{}_{}.npy'.format(hparams.classifier, 'train'))
-        save_labels(file_path, labels)
+        labels = evaluate_for_dataset(module.model, train_dataloader, probabilities=hparams.probabilities, gpu=not hparams.no_gpu)
+        os.makedirs(folder, exist_ok=True)
+        file_path = os.path.join(folder, '{}_{}.npy'.format(hparams.classifier, 'train'))
+        save_labels(file_path, labels, probabilities=hparams.probabilities)
 
     if hparams.test:
         test_dataset = CIFAR10(hparams.data_dir, train=False, download=False, transform=transform_dataset)
         test_dataloader = DataLoader(test_dataset, batch_size=hparams.batch_size, num_workers=4, shuffle=False, drop_last=False, pin_memory=True)
         print('Evaluate for test dataset')
-        labels = evaluate_for_dataset(module.model, test_dataloader, gpu=not hparams.no_gpu)
-        file_path = os.path.join('labels', '{}_{}.npy'.format(hparams.classifier, 'test'))
-        save_labels(file_path, labels)
+        labels = evaluate_for_dataset(module.model, test_dataloader, probabilities=hparams.probabilities, gpu=not hparams.no_gpu)
+        file_path = os.path.join(folder, '{}_{}.npy'.format(hparams.classifier, 'test'))
+        save_labels(file_path, labels, probabilities=hparams.probabilities)
 
-def evaluate_for_dataset(model, dataloader, gpu=True):
+def evaluate_for_dataset(model, dataloader, probabilities=False, gpu=True):
     result = []
     total = len(dataloader)
     with torch.no_grad():
@@ -61,18 +67,26 @@ def evaluate_for_dataset(model, dataloader, gpu=True):
             if gpu:
                 images = images.cuda()
             predictions = model(images)
-            result += predictions.argmax(axis=1).tolist()
+            if probabilities:
+                current_result = softmax(predictions, dim=-1).tolist()
+            else:
+                current_result = predictions.argmax(axis=1).tolist()
+            result += current_result
     return result
 
-def save_labels(file_path, labels):
+def save_labels(file_path, labels, probabilities=False):
     with open(file_path, 'wb') as f:
-        np.save(f, np.array(labels, dtype=np.int8))
+        if probabilities:
+            np.save(f, np.array(labels, dtype=np.float32))
+        else:
+            np.save(f, np.array(labels, dtype=np.int8))
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--classifier', type=str, default='resnet18')
     parser.add_argument('--data_dir', type=str, default='/data/huy/cifar10/')
     parser.add_argument('--no_gpu', action='store_true')
+    parser.add_argument('--probabilities', action='store_true')
     parser.add_argument('--gpus', default='0,')
     parser.add_argument('--max_epochs', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=256)
