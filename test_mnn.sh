5bc52bb50cf289d26bd7ab11fb291f84aed46692
Aleksandr Zuev <zuev08@gmail.com>
2021-02-03 17:37:33 +0100

    Commit training parameters

--- test_mnn.sh
+++ test_mnn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 mnn_test.py --classifier mobilenet_v2 --data_dir dataset --gpus 0,
+python3 mnn_test.py --classifier mobilenet_v2 --data_dir dataset --gpus 0, --switch_threshold 0.18
