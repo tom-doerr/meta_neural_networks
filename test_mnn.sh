2b8b0979cc4352d5d5492ba6accb5147a19d9d2e
Aleksandr Zuev <zuev08@gmail.com>
2021-02-04 09:51:35 +0100

    Add passing switch_threshold to test_mnn* script

--- test_mnn.sh
+++ test_mnn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 mnn_test.py --classifier mobilenet_v2 --data_dir dataset --gpus 0, --switch_threshold 0.18
+python3 mnn_test.py --classifier mobilenet_v2 --data_dir dataset --gpus 0, --switch_threshold ${1:-0.25}
